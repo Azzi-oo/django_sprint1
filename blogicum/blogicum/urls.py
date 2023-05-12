21064bd63059acbd4cf678a3bb6e5746aded4905
Азат Янбердин <yanberdin.az@yandex.ru>
2023-05-12 17:20:50 +0500

    new change

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('',include('blog.urls'), name='blog'),
-    path('',include('pages.urls'), name='pages'),
+    path('', include('blog.urls'), name='blog'),
+    path('', include('pages.urls'), name='pages'),
     path('admin/', admin.site.urls),
 ]
