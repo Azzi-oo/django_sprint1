be26ba19e589ee12fb054e0e556572f5bd41d90f
Азат Янбердин <yanberdin.az@yandex.ru>
2023-05-14 14:06:09 +0500

    new change

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('blog.urls'), name='blog'),
-    path('', include('pages.urls'), name='pages'),
+    path('', include('blog.urls', namespace='blog')),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
 ]
